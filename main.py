5ab1a2dafb5f8b9def787c88b8129f19f3ef2313
Fentipluh <bigstar0295@gmail.com>
2024-02-28 13:52:35 +0700

    first working variant

--- main.py
+++ main.py
@@ -2,36 +2,64 @@ import sys
 import random
 import numpy as np
 
-def find_neighbors_distance_1(vector):
-    neighbors = []
-    n = len(vector)
+def hamming_distance(y1, y2):
+    return np.count_nonzero(y1 != y2)
+def hamming_weight(y):
+    return np.count_nonzero(y)
+def k_swap_neighborhood(y, r, k):
+    neighborhoods = []
+    n = len(y)
     for i in range(n):
-        neighbor = vector.copy()
-        neighbor[i] = 1 - neighbor[i]
-        neighbors.append(neighbor)
-    return neighbors
-def find_neighbors_distance_2(vector):
-    neighbors = []
-    n = len(vector)
-    for i in range(n):
-        for j in range(i + 1, n):
+        if y[i]:  # Facility is open
+            for j in range(n):
+                if not y[j]:  # Facility is closed
+                    y_swap = np.copy(y)
+                    y_swap[i] = False
+                    y_swap[j] = True
+                    if hamming_weight(y_swap) == r and hamming_distance(y, y_swap) == 2 * k:
+                        neighborhoods.append(y_swap)
+    return neighborhoods
+
+def k_shake(vector, k):
+    if k == 1:
+        neighbors = []
+        n = len(vector)
+        for i in range(n):
             neighbor = vector.copy()
-            neighbor[i] = 1 - neighbor[i]  # Инвертирование i-го элемента
-            neighbor[j] = 1 - neighbor[j]  # Инвертирование j-го элемента
+            neighbor[i] = 1 - neighbor[i]
             neighbors.append(neighbor)
-    return neighbors
-def find_neighbors_distance_3(vector):
-    neighbors = []
-    n = len(vector)
-    for i in range(n):
-        for j in range(i + 1, n):
-            for k in range(j + 1, n):
+
+
+    if k == 2:
+        neighbors = []
+        n = len(vector)
+        for i in range(n):
+            for j in range(i + 1, n):
                 neighbor = vector.copy()
                 neighbor[i] = 1 - neighbor[i]  # Инвертирование i-го элемента
                 neighbor[j] = 1 - neighbor[j]  # Инвертирование j-го элемента
-                neighbor[k] = 1 - neighbor[k]  # Инвертирование k-го элемента
-                neighbors.append(neighbor) #.tolist()
+                neighbors.append(neighbor)
+
+    if k == 3:
+        neighbors = []
+        n = len(vector)
+        for i in range(n):
+            for j in range(i + 1, n):
+                for k in range(j + 1, n):
+                    neighbor = vector.copy()
+                    neighbor[i] = 1 - neighbor[i]  # Инвертирование i-го элемента
+                    neighbor[j] = 1 - neighbor[j]  # Инвертирование j-го элемента
+                    neighbor[k] = 1 - neighbor[k]  # Инвертирование k-го элемента
+                    neighbors.append(neighbor)  # .tolist()
     return neighbors
+def find_best_neighbor_SP(neighbors):
+    best_vector = neighbors[0]
+    for i in range(len(neighbors)):
+        if (SP(neighbors[i]) > SP(best_vector)):
+            best_vector = neighbors[i]
+    return best_vector
+
+
 def find_new_b(b, c, vector):
     strings = np.where(vector == 1)[0]
     new_c = c[strings, :]
@@ -85,20 +113,7 @@ def generate_first_vector(n, facility_amount):
     indices = np.random.choice(range(n), facility_amount, replace=False)  # Выбираем случайные индексы без повторений
     vector[indices] = 1  # Устанавливаем единички в выбранных индексах
     return vector
-def find_X(b, c, p, ro, vector):
-    result = np.zeros((n, m))
 
-    for j in range(len(b)):
-        result[:, j] = b[j] - c[:, j] - p - ro
-    max_value = np.max(result, axis=0)
-    # Создаем матрицу x
-    x = np.zeros(result.shape, dtype=int)
-    for j in range(result.shape[1]):
-        if max_value[j] > 0:
-            max_indices = np.where(result[:, j] == max_value[j])[0]
-            x[max_indices, j] = 1
-    #x = np.multiply(x, vector.reshape(-1, 1))
-    return x
 
 def first_criterion(f , vector):
     new_b = find_new_b(b, c, vector)
@@ -112,6 +127,17 @@ def first_criterion(f , vector):
 def generate_f(n):
     random.seed(42)
     return np.random.randint(0, 100, size=n)
+def local_search_RF(y):
+    best_y = y
+    neighbors = k_swap_neighborhood(y, 5, 1)
+    best_b = find_new_b(b, c, best_y)
+    for neighbor in neighbors:
+        temp_b = find_new_b(b, c, neighbor)
+        if(RF(temp_b) >= RF(best_b)):
+            best_y = neighbor
+            best_b = temp_b
+    return best_y
+
 
 
 # Данные для задачи
@@ -125,103 +151,24 @@ facility_amount = int(numbers[2])  # кол-во открываемых пред
 
 b = np.loadtxt('input_b.txt')
 c = np.loadtxt('input_c.txt')
-T = 100
+T = 10
 f = generate_f(n)
 V = 100
 
 
-def generate_f(n):
-    random.seed(42)
-    return np.random.randint(0, 100, size=n)
+def VND_2(Imax, k):
+    I = 0
+    y = generate_first_vector(n,5)  # Step 0: Generate initial Boolean vector y
+    print(RF(find_new_b(b,c,y)))
+    print(y)
+    while I < Imax:
+        y_star = local_search_RF(y)  # Step 1: Apply local search to 1-Swap(y)
+        I += 1
+        if I > Imax or hamming_distance(y, y_star) == 0:
+            break  # If no improvement or maximum iterations reached, stop
+    return y_star  # Return the best found facility location y
 
-
-
-def VND():
-    T = 100
-    i = 0
-    for i in range(T):
-        if i == 0:
-            first_vector = generate_first_vector(n, facility_amount)
-            first_new_b = find_new_b(b, c, first_vector)
-            first_price = SP(first_new_b)
-            best_price = first_price
-            best_vector = first_vector
-            neighbors = find_neighbors_distance_1(best_vector)
-        if i > 0:
-            for j in range(len(neighbors)):
-                if(first_criterion(f, neighbors[j])) > first_criterion(f, best_vector):
-                    print(first_criterion(f, neighbors[j]))
-                    print(first_criterion(f,best_vector))
-                    best_vector = neighbors[j]
-                    best_price = SP(find_new_b(b, c, best_vector))
-
-
-
-
-
-
-
-
-
-
-
-def generate_f(n):
-    random.seed(42)
-    return np.random.randint(0, 100, size=n)
-
-
-
-
-
-VND()
-
-
-
-
-#np.set_printoptions(threshold=sys.maxsize)
-
-
-# first_vector = generate_first_vector(n,facility_amount)
-# first_new_b = find_new_b(b,c, first_vector)
-# best_price = SP(first_new_b)
-# best_ro = RF(first_new_b)
-# best_vector = first_vector
-# print(best_price, best_vector, np.sum(best_vector), best_ro)
-# for vector in find_neighbors_distance_1(first_vector):
-#     new_vector_b = find_new_b(b, c, vector)
-#     temp_price = SP(new_vector_b)
-#     temp_ro = RF(new_vector_b)
-#     temp_vector = vector
-#     if temp_price > best_price:
-#         best_price = temp_price
-#         best_vector = temp_vector
-#     best_ro = RF(new_vector_b)
-# print(best_price, best_vector, np.sum(best_vector), best_ro)
-# for vector in find_neighbors_distance_2(best_vector):
-#
-#     new_vector_b = find_new_b(b, c, vector)
-#     temp_price = SP(new_vector_b)
-#     temp_ro = RF(new_vector_b)
-#     temp_vector = vector
-#     if temp_price > best_price:
-#         best_price = temp_price
-#         best_vector = temp_vector
-#     best_ro = RF(new_vector_b)
-# print(best_price, best_vector, np.sum(best_vector), best_ro)
-# for vector in find_neighbors_distance_3(best_vector):
-#     new_vector_b = find_new_b(b, c, vector)
-#     temp_price = SP(new_vector_b)
-#     temp_ro = RF(new_vector_b)
-#     temp_vector = vector
-#     if temp_price > best_price:
-#         best_price = temp_price
-#         best_vector = temp_vector
-#     best_ro = RF(new_vector_b)
-#
-# print(best_price, best_vector, np.sum(best_vector), best_ro)
-
-
-
-
-
-
+y_star = VND_2(100, 1)
+print(y_star)
+b_star = find_new_b(b,c,y_star)
+print(RF(b_star))
